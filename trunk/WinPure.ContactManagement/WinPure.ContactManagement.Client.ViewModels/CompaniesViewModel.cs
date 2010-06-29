a680bafdc49fe7490d45ffa1e8e65db920387bd7
ishikht <ishikht@2c8acd95-ff5e-04cd-6afe-5de189941a37>
2010-06-29 12:02:39 +0000

    Added ViewModels.

---
+++ trunk/WinPure.ContactManagement/WinPure.ContactManagement.Client.ViewModels/CompaniesViewModel.cs
@@ -0,0 +1,50 @@
+﻿#region References
+
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using WinPure.ContactManagement.Client.Data.Managers;
+using WinPure.ContactManagement.Client.Data.Model;
+
+#endregion
+
+namespace WinPure.ContactManagement.Client.ViewModels
+{
+    public class CompaniesViewModel : ViewModelBase
+    {
+        #region Fields
+
+        private ObservableCollection<Company> _companies;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public CompaniesViewModel()
+        {
+            Companies = CompaniesManager.Current.LoadCompanies();
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Collection of Companies.
+        /// </summary>
+        public ObservableCollection<Company> Companies
+        {
+            get { return _companies; }
+            set
+            {
+                if (_companies == value) return;
+                _companies = value;
+                RaisePropertyChanged("Companies");
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/WinPure.ContactManagement/WinPure.ContactManagement.Client.ViewModels/ContactsViewModel.cs
@@ -0,0 +1,50 @@
+﻿#region References
+
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using WinPure.ContactManagement.Client.Data.Managers;
+using WinPure.ContactManagement.Client.Data.Model;
+
+#endregion
+
+namespace WinPure.ContactManagement.Client.ViewModels
+{
+    public class ContactsViewModel : ViewModelBase
+    {
+        #region Fields
+
+        private ObservableCollection<Contact> _contacts;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ContactsViewModel()
+        {
+            Contacts = ContactsManager.Current.LoadContacts();
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Collection of Contacts.
+        /// </summary>
+        public ObservableCollection<Contact> Contacts
+        {
+            get { return _contacts; }
+            set
+            {
+                if (_contacts == value) return;
+                _contacts = value;
+                RaisePropertyChanged("Contacts");
+            }
+        }
+
+        #endregion
+    }
+}
