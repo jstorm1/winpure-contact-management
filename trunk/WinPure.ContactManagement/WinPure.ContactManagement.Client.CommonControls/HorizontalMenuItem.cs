6539767209b5390f69df078a63c3a4bd9683273d
ishikht <ishikht@2c8acd95-ff5e-04cd-6afe-5de189941a37>
2010-06-29 16:08:58 +0000

    Added Horizontal menu.

---
+++ trunk/WinPure.ContactManagement/WinPure.ContactManagement.Client.CommonControls/HorizontalMenuItem.cs
@@ -0,0 +1,56 @@
+﻿#region References
+
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+#endregion
+
+namespace WinPure.ContactManagement.Client.CommonControls
+{
+    public class HorizontalMenuItem : Button
+    {
+        #region Dependency properties
+
+        // Using a DependencyProperty as the backing store for Image.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof (ImageSource), typeof (HorizontalMenuItem), null);
+
+
+        // Using a DependencyProperty as the backing store for Content.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.RegisterAttached("Text", typeof (string), typeof (HorizontalMenuItem),
+                                                new UIPropertyMetadata("Menu Item"));
+
+        #endregion
+
+        #region Constructor
+        
+        static HorizontalMenuItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HorizontalMenuItem),
+                                                     new FrameworkPropertyMetadata(typeof(HorizontalMenuItem)));
+        } 
+
+        #endregion
+
+        #region Properties
+
+        [Category("Common Properties")]
+        public ImageSource Image
+        {
+            get { return (ImageSource) GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        [Category("Common Properties")]
+        public string Text
+        {
+            get { return (string) GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        #endregion
+    }
+}
