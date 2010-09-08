9723359ec286688ab9910774a7fae77b1c27c474
ishikht <ishikht@2c8acd95-ff5e-04cd-6afe-5de189941a37>
2010-09-08 09:45:56 +0000

    Added DetailsView Style.

--- trunk/WinPure.ContactManagement/WinPure.ContactManagement.Client.CommonControls/ListView/Views/DetailsView.cs
+++ trunk/WinPure.ContactManagement/WinPure.ContactManagement.Client.CommonControls/ListView/Views/DetailsView.cs
@@ -2,14 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace WinPure.ContactManagement.Client.CommonControls.ListView.Views
 {
     /// <summary>
     /// Custom Details View for the <see cref="ListView"/> control.
+    /// 
+    /// ImageView displays image files using themselves as their icons.
+    /// In order to write our own visual tree of a view, we should override its
+    /// DefaultStyleKey and ItemContainerDefaultKey. DefaultStyleKey specifies
+    /// the style key of <see cref="ListView"/>; ItemContainerDefaultKey specifies the style
+    /// key of <see cref="ListViewItem"/>.
     /// </summary>
     public class DetailsView:ViewBase
     {
+        #region DefaultStyleKey
+
+        protected override object DefaultStyleKey
+        {
+            get { return new ComponentResourceKey(GetType(), "ImageView"); }
+        }
+
+        #endregion
+
+        #region ItemContainerDefaultStyleKey
+
+        protected override object ItemContainerDefaultStyleKey
+        {
+            get { return new ComponentResourceKey(GetType(), "ImageViewItem"); }
+        }
+
+        #endregion
+
     }
 }
