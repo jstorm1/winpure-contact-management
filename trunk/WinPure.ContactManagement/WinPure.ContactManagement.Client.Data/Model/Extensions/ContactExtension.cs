199f78cd2c1ce3132775c5d10354af9dcba1f0b4
nazar.grynko@gmail.com <nazar.grynko@gmail.com@2c8acd95-ff5e-04cd-6afe-5de189941a37>
2010-10-29 16:18:30 +0000

    For receiving columns names

---
+++ trunk/WinPure.ContactManagement/WinPure.ContactManagement.Client.Data/Model/Extensions/ContactExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace WinPure.ContactManagement.Client.Data.Model.Extensions
+{
+    public static class ContactExtension
+    {
+        /// <summary>
+        /// Get column names of contacts
+        /// </summary>
+        /// <returns>List on column names</returns>
+        public static IEnumerable<string> GetContactsColumnNames(this Contact contact)
+        {
+            // Not shown columns
+            var hideProperties = new List<string>
+                                     {
+                                         "ContactID",
+                                         "CompanyId",
+                                         "Company",
+                                         "CompanyReference",
+                                         "HasErrors",
+                                         "Item",
+                                         "Error",
+                                         "EntityState",
+                                         "EntityKey"
+                                     };
+
+            PropertyInfo[] properties = typeof(Contact).GetProperties();
+
+            return from property in properties where !hideProperties.Contains(property.Name) select property.Name;
+        }
+    }
+}
